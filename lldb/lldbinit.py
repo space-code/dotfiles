0b521b572c370175d512ad73db6e0e6209ded382
Nikita Vasilev <nv3212@gmail.com>
2025-05-08 16:15:48 +0500

    Create the `commands` dir

--- lldb/lldbinit.py
+++ lldb/lldbinit.py
@@ -9,14 +9,15 @@ import os
 def __lldb_init_module(debugger, internal_dict):
     file_path = os.path.realpath(__file__)
     dir_name = os.path.dirname(file_path)
-    load_python_scripts_dir(dir_name)
+    commands_direcory = os.path.join(dir_name, "commands")
+    load_python_scripts_dir(commands_direcory)
 
 def load_python_scripts_dir(dir_name):
     this_files_basename = os.path.basename(__file__)
     cmd = ''
     for file in os.listdir(dir_name):
         if file.endswith('.py'):
-            cmd = 'command script import ' 
+            cmd = 'command script import '
         elif file.endswith('.txt'):
             cmd = 'command source -e0 -s1 '
         else: 
